fbeffb56c30bb314f0a9f76ea01f202c5eb6a896
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-10-17 10:08:57 +0700

    change

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,7 +3,6 @@ package main.java.server;
 import java.io.*;
 import java.util.logging.*;
 import java.net.*;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,59 +10,26 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Server implements Runnable {
-    private int thisThreadNumber; 
+	protected Logger logger;
+	private int thisThreadNumber; 
     private Socket socket;
     private EveryThreeSecondTimer timer;
     private static AtomicInteger globalNumber = new AtomicInteger(-1); 
-    private static final Logger logger = Logger.getLogger(Server.class.getName());
-    
     private ScheduledExecutorService sceduledThreadPool = Executors.newScheduledThreadPool(1);
-    private static ExecutorService threadPool = Executors.newCachedThreadPool();
-    
-    private static final int SERVER_ARGS_COUNT = 1;
+
     private static final long REPEAT_LOG_TIME = 3000;   
     private static final long MILLS_TO_SEC = 1000;   
     
     
-    public Server(Socket socket) {        
+    public Server(Logger logger, Socket socket) {   
+    	this.logger = logger;
         this.socket = socket;
         this.thisThreadNumber = globalNumber.incrementAndGet();
         this.timer = new EveryThreeSecondTimer(logger, thisThreadNumber);
     }
 
-    
-    public static void main(String[] args) {
-    	
-    	LogManager logManager = LogManager.getLogManager();
-        try {
-            logManager.readConfiguration(new FileInputStream("src/main/resources/logServer.properties"));
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE, "Cannot get log configuration!" + ex.getMessage());
-        }
-    	
-    	if (args.length < SERVER_ARGS_COUNT) {
-            logger.log(Level.SEVERE, "Not enough arguments\\nYou should type port numbert");
-            System.exit(1);
-        }
-    	
-        int port = Integer.parseInt(args[0]);
-        
-        try {
-            try (ServerSocket serverSocket = new ServerSocket(port)) {
-		logger.log(Level.INFO, "The server started working");
-      
-		while (!serverSocket.isClosed()) {
-		    Server myServer = new Server(serverSocket.accept());                                     
-		    threadPool.submit(() -> myServer.run());  
-		}
-	    }
-        }
-        catch (Exception e) {
-            logger.log(Level.SEVERE, "Can't receive connections: " + e.getMessage());
-	}    	       
-    }
-    
-    private long getFileAndSaveTime(InputStream socketIn, FileOutputStream fileStream, long fileSize) throws SocketException, IOException {
+
+	private long getFileAndSaveTime(InputStream socketIn, FileOutputStream fileStream, long fileSize) throws SocketException, IOException {
     	long periodStart = System.currentTimeMillis(), start = periodStart ;
         long allCount = 0, speedCount = 0;
         byte[] buf = new byte[8192];
@@ -99,6 +65,11 @@ public class Server implements Runnable {
            
             String fileName = socketDataIn.readUTF();
             long fileSize = socketDataIn.readLong();
+            
+            if (new File("uploads/" + fileName).isFile()) {
+            	logger.log(Level.INFO, "This file already exists");
+            	System.exit(0);
+            }
 
             downloadedFile = new File("uploads/" + fileName);
             downloadedFile.getParentFile().mkdirs();
@@ -110,8 +81,8 @@ public class Server implements Runnable {
             
             byte msg = 100;
        
-    	    socketOut.write(msg);
-    	    fileStream.close();
+    		socketOut.write(msg);
+    		fileStream.close();
             logger.log(Level.INFO, "Server Thread №" + thisThreadNumber + ": Successfully downloaded " + fileName + " with average speed " + MILLS_TO_SEC * fileSize / time + " bytes per second in " + time / 1000.0 + " seconds");
             
             timer.setLocalTime(time);
@@ -121,7 +92,7 @@ public class Server implements Runnable {
             sceduledThreadPool.shutdown();
         }
         catch (SocketException e) {
-            sceduledThreadPool.shutdown();
+        	sceduledThreadPool.shutdown();
         	
             if (fileStream != null) {
                 try {
@@ -139,11 +110,11 @@ public class Server implements Runnable {
             
         }
         catch (IOException e) {
-            sceduledThreadPool.shutdown();
+        	sceduledThreadPool.shutdown();
             logger.log(Level.SEVERE, "Server Thread №" + thisThreadNumber + ": Connection error: " + e.getMessage());
         }
         finally {
-            globalNumber.decrementAndGet();
+        	globalNumber.decrementAndGet();
         }
     }
 }
