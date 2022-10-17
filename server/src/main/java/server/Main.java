eb9d681ac140d3ec6ad0689c7091ee4712d4c637
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-10-17 10:13:39 +0700

    change

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -10,45 +10,43 @@ import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
-	private final static Logger logger = Logger.getLogger(Main.class.getName());
-	private static final int SERVER_ARGS_COUNT = 1;
-	private static ExecutorService threadPool = Executors.newCachedThreadPool();
+    private final static Logger logger = Logger.getLogger(Main.class.getName());
+    private static final int SERVER_ARGS_COUNT = 1;
+    private static ExecutorService threadPool = Executors.newCachedThreadPool();
 	
 	
-	 public static void main(String[] args) {
+    public static void main(String[] args) {	    	
+	LogManager logManager = LogManager.getLogManager();
+	try {
+	     logManager.readConfiguration(new FileInputStream("src/main/resources/logServer.properties"));
+	} catch (IOException ex) {
+	     logger.log(Level.SEVERE, "Cannot get log configuration!" + ex.getMessage());
+	}
 	    	
-	    	LogManager logManager = LogManager.getLogManager();
-	        try {
-	            logManager.readConfiguration(new FileInputStream("src/main/resources/logServer.properties"));
-	        } catch (IOException ex) {
-	            logger.log(Level.SEVERE, "Cannot get log configuration!" + ex.getMessage());
-	        }
+	if (args.length < SERVER_ARGS_COUNT) {
+	     logger.log(Level.SEVERE, "Not enough arguments\nYou should type port number");
+	     System.exit(1);
+	}    	
 	    	
-	    	if (args.length < SERVER_ARGS_COUNT) {
-	            logger.log(Level.SEVERE, "Not enough arguments\nYou should type port number");
-	            System.exit(1);
-	        }    	
+	int port = 0;
 	    	
-	    	int port = 0;
+	try {
+	    port = Integer.parseInt(args[0]);
+	 } catch (NumberFormatException ex) {
+	     logger.log(Level.SEVERE, ex.getMessage() + ". It can't be port number, please write integer");
+	     System.exit(1);
+	 }
 	    	
-	    	try {
-	            port = Integer.parseInt(args[0]);
-	    	} catch (NumberFormatException ex) {
-	    		logger.log(Level.SEVERE, ex.getMessage() + ". It can't be port number, please write integer");
-	    		System.exit(1);
-	    	}
-	    	
-			try (ServerSocket serverSocket = new ServerSocket(port)) {
-				logger.log(Level.INFO, "The server started working");
+	try (ServerSocket serverSocket = new ServerSocket(port)) {
+	      logger.log(Level.INFO, "The server started working");
 	      
-				while (!serverSocket.isClosed()) {
-					Server myServer = new Server(logger, serverSocket.accept());                                     
-					   threadPool.submit(() -> myServer.run());  
-				}
-				
-	        }
-	        catch (Exception e) {
-	            logger.log(Level.SEVERE, "Can't receive connections: " + e.getMessage());
-			}    	       
-	    }
+	      while (!serverSocket.isClosed()) {
+		   Server myServer = new Server(logger, serverSocket.accept());                                     
+		   threadPool.submit(() -> myServer.run());  
+	      }
+	}
+	catch (Exception e) {
+	       logger.log(Level.SEVERE, "Can't receive connections: " + e.getMessage());
+	}    	       
+    }
 }
